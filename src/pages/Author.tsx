259d3b1ea7a07f588a5bd193f4db2e131927465e
Neil Smith <me@neilsmith.fyi>
2022-02-12 15:40:11 -0600

    add author page
    Rouge draft complete. I have a Posts page which displays the title and
    body of all posts, a Post page which displays the author as a link and
    a list of comments in addition to the title and body of the post, and
    an Author page which displays only the posts for the current author.

--- src/pages/Author.tsx
+++ src/pages/Author.tsx
@@ -1,3 +1,30 @@
+import { useMatch } from "react-router-dom";
+import { usePosts } from "app/app.hooks";
+import { CardLink } from "components/Card";
+import { NewLineText } from "components/NewLineText";
+
 export const AuthorPage = () => {
-  return <div>author page</div>;
+  const match = useMatch("/author/:id");
+  const authorIdParam = match?.params.id || "0"; // todo: handle bad match?
+  const authorId = +authorIdParam;
+
+  const { isLoading, error, data } = usePosts();
+
+  // todo: handle loading, error, no posts found
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>{`An error has occured: ${error.message}`}</p>;
+  if (!data) return <p>No posts found...</p>;
+
+  const posts = data.filter((post) => post.userId === authorId);
+
+  return (
+    <>
+      {posts.map((post) => (
+        <CardLink key={post.id} elementType="article" to={`/post/${post.id}`}>
+          <h1>{post.title}</h1>
+          <NewLineText content={post.body} />
+        </CardLink>
+      ))}
+    </>
+  );
 };
